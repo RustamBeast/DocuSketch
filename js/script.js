8d002981d62b09eb95b87725f20beceee2bd7686
RustamBeast <rusyaf0608@mail.ru>
2022-03-05 14:39:07 +0100

    Responsive + rotate 'L' shaped room around itself

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ const light = new THREE.AmbientLight(0xffffff);
 scene.add(light);
 
 // First room object (rectangle form)
-const geometryForRectangle = new THREE.PlaneGeometry(400, 200);
+const geometryForRectangle = new THREE.PlaneGeometry(width/8, height/16);
 const materialForRectangle = new THREE.MeshBasicMaterial({color: 0x00ca00});
 const meshOfRectangle = new THREE.Mesh(geometryForRectangle, materialForRectangle);
 meshOfRectangle.position.x = -width / 4;
@@ -33,15 +33,20 @@ scene.add(meshOfRectangle);
 
 // Second room object ("L" form)
 const LShape = new THREE.Shape();
-LShape.moveTo(0,150)
-LShape.lineTo(75, 150);
-LShape.lineTo(75, 0);
-LShape.lineTo(150, 0);
-LShape.lineTo(150, -75);
-LShape.lineTo(0, -75);
+LShape.lineTo(width/32, 0);
+LShape.lineTo(width/32, -height/32);
+LShape.lineTo(width/16, -height/32);
+LShape.lineTo(width/16, -height/16);
+LShape.lineTo(0, -height/16);
 const geometryForLShape = new THREE.ShapeGeometry(LShape);
+console.log(geometryForLShape.center());
 const materialForLShape = new THREE.MeshBasicMaterial({color: 0x0000bb});
 const meshForLShape = new THREE.Mesh(geometryForLShape, materialForLShape);
+
+// Create a box to rotate the room around itself instead of global axis
+const box = new THREE.Box3().setFromObject( meshForLShape );
+box.center( meshForLShape.position ); 
+meshForLShape.position.multiplyScalar( - 1 );
 meshForLShape.position.x = width / 4;
 scene.add(meshForLShape);
 
